004fe3b5bb92e61a4eefd772c6ba72b3fd607ad3
Richiearnold <richard@ambercouch.co.uk>
2019-01-03 11:37:23 +0000

    Added destination-child-list__item class to static li

--- destination-child-list-template.php
+++ destination-child-list-template.php
@@ -1,7 +1,7 @@
 
 <div class="destination-child-list">
   <ul class="destination-child-list__list">
-    <li><a href="<?php echo get_term_link( $term_oldest_parent, 'destination') ?>">All</a></li>
+    <li class="destination-child-list__item" ><a href="<?php echo get_term_link( $term_oldest_parent, 'destination') ?>"><?php echo $term_oldest_parent->name; ?>  </a></li>
     <?php foreach (  $child_terms as $child ) :
     $term = get_term_by( 'id', $child, 'destination'); ?>
         <li class="destination-child-list__item">

ed2396329e3a5b0658e7bb4cd0e52104fb03f698
Richie Arnold <richard@ambercouch.co.uk>
2019-01-08 14:39:13 +0000

    Rename destination header template

---
+++ term-header-template.php
@@ -0,0 +1,9 @@
+
+<div class="term-header <?php echo $term_slug; ?>-header <?php echo $tax_slug; ?>-header">
+  <div class="term-header__image <?php echo $term_slug; ?>-header__image">
+    <img src="<?php echo $term_image['sizes']['location-header']; ?>" alt="<?php echo $term_name; ?>" class="term-header_img <?php echo $term_slug; ?>-header_img">
+  </div>
+  <div class="term-header__title-image <?php echo $term_slug; ?>-header__title-image">
+    <img src="<?php echo $term_title_image['url']; ?>" alt="<?php echo $term_name; ?>" class="term-header__title-img <?php echo $term_slug; ?>-header__title-img">
+  </div>
+</div>

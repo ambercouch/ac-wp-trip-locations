acabad916a817187dd6a26612223f2ea9860b095
Richie Arnold <richard@ambercouch.co.uk>
2019-01-08 14:41:47 +0000

    Added thumb templates

---
+++ subject-icon-thumb.php
@@ -0,0 +1,13 @@
+
+<div class="subject-thumb">
+  <div class="subject-thumb__content">
+    <a href="<?php echo $term_link; ?>" class="subject__link">
+    <div class="subject-thumb__image">
+      <img src="<?php echo $subject_icon['sizes']['thumbnail'] ?>" alt="<?php echo $subject_title ?>" class="subject-thumb__img">
+    </div>
+    <div class="subject-thumb__title">
+      <h4 class="subject-thumb__heading"><?php echo $subject_title ?></h4>
+    </div>
+    </a>
+  </div>
+</div>
---
+++ subject-thumb.php
@@ -0,0 +1,13 @@
+
+<div class="tax-thumb <?php echo $tax ?>-thumb">
+  <div class="tax-thumb__content">
+    <a href="<?php echo $tax_link; ?>" class="tax-thumb__link">
+    <div class="tax-thumb__images">
+      <img src="<?php echo $tax_image ?>" alt="<?php echo $tax_title ?>" class="tax-thumb__img">
+    </div>
+    <div class="tax-thumb__title">
+      <h2 class="tax-thumb__heading"><?php echo $tax_title ?></h2>
+    </div>
+    </a>
+  </div>
+</div>
